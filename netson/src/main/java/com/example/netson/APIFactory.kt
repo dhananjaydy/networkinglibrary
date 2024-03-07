291704a045a08099f6f36aa245b1de3b0a605046
dhananjay <djchawla555@gmail.com>
2024-03-07 12:36:43 +0530

    modyfying client usage

--- netson/src/main/java/com/example/netson/APIFactory.kt
+++ netson/src/main/java/com/example/netson/APIFactory.kt
@@ -4,8 +4,12 @@ import okhttp3.OkHttpClient
 
 object APIFactory {
 
-    inline fun <reified T> provideRetrofit(endPoint: String, okHttpClient: OkHttpClient): T {
-        val retrofit = RetrofitHelper.provideRetrofit(endPoint, okHttpClient)
-        return retrofit.create(T::class.java)
+    inline fun <reified T> provideRetrofitService(baseUrl: String): T {
+        return RetrofitHelper.provideRetrofit(baseUrl, provideClient()).create(T::class.java)
     }
+
+    fun provideClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
 }
